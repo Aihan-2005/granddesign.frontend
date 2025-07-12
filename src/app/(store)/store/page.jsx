3d8e56e373ccbb45129f91aef5676926c7250b5f
kindboy <kindboy2025@gmail.com>
2025-07-12 13:25:46 +0330

    delete tsconfig

--- src/app/(store)/store/page.jsx
+++ src/app/(store)/store/page.jsx
@@ -1,7 +1,18 @@
+"use client"
+
+import { useTestStore } from "@/zustand/store"
+
+// import { useTestStore } from "../../../zustand/store.js"
+
+
+
+
+
 export default function StorePage(){
+     const {count} = useTestStore()
     return (
         <>
-         <div>start Store</div>
+         <div>{count}</div>
          </>
     )
 }
