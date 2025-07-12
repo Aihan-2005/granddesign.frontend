5ad71662c91199838669ecb46ba965139e632fef
kindboy <kindboy2025@gmail.com>
2025-07-12 19:00:41 +0330

    add slider projects

--- src/app/(store)/home/page.jsx
+++ src/app/(store)/home/page.jsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import BackgroundImage from "./backgroundImage";
 import BackgroundeAboutUs from "./backgoroundAboutUs";
+import SliderProjects from "./sliderProjects";
 
 export default function HomePage(){
     return (
         <>
 <BackgroundImage/>
+<div className="px-[50px]">
 <BackgroundeAboutUs/>
+<SliderProjects />
+</div>
          </>
     )
 }
