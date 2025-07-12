65a1beb350ef868023b49e1d3fc9a5244895d849
kindboy <kindboy2025@gmail.com>
2025-07-12 19:00:41 +0330

    add slider projects

--- src/app/(store)/home/backgoroundAboutUs.jsx
+++ src/app/(store)/home/backgoroundAboutUs.jsx
@@ -1,17 +1,35 @@
 export default function BackgroundeAboutUs(){
     return (
         <>
-          <div className="w-[100%] h-[100vh] relative flex justify-center items-end">
-               <div className="w-[40%] h-[100%]  flex justify-between items-center">
-                <div>
-                <p className="text-[40px] my-1 text-[white] font-bold">طراحی مدرن خانه </p>
-                <p className="text-[16px]  my-1 text-[#e4e4e4]">تجربه ای متفات و خاص با تیم با تجربه ما</p>
-                <button className="w-[150px] cursor-pointer my-2 text-[14px] py-3 bg-[green] text-white rounded-lg transation duration-200 hover:scale-[1.1] ">همین اکنون شروع کنید </button>
-           </div>
-            </div>
-                 <img src="images/homepage.png" alt=""  className="w-[60%] h-[100%]"/>
-    
-         </div>
+<div className="w-full h-[90vh] my-5 relative flex justify-center items-end " dir="rtl">
+
+      <img
+    src="images/aboutimage.png"
+    alt="About Us"
+    className="w-[40%] h-full object-cover"
+  />
+  <div className="w-[60%] h-full flex justify-between items-center">
+    <div className="w-full text-right  px-5 relative">
+<p className="text-3xl my-1 text-green-500 font-bold py-3 drop-shadow-[2px_2px_0px_#fff]">
+  درباره ما
+</p>      <p className="text-4xl my-1 text-[#e4e4e4] py-3">چرا ما متفاوت هستیم</p>
+      <p className="text-white w-[100%]  pl-[19%] py-3 border-r-[2px] px-2 border-[#e31e1e]">
+        تجربه‌ای خاص و متفاوت با تیم درجه یک و با تجربه، به کمک بروزترین ابزارهای دنیا بهترین خانه‌ها را با ما تجربه کنید.
+      </p>
+      <p className="text-white w-[100%]  pl-[19%] py-3 px-2">
+      حس خانه ای مدرن با کمک ابزار بروز دنیا همچون هوشمصنوعی خانه هایی خاص خوتهیم ساخت مه در ایران نمییتوانید ببینید
+      </p>
+      <p className="text-white w-[100%]  pl-[19%] py-3 px-2">
+       حس ها جدید و دل انگیز با کمک خانه های مدرن و ایده های ما برای هرچه زیبا تر کردن زندگی شما ، مارا انتخاب کنید همچون یک دوست
+      </p>
+      <button className="w-[150px] cursor-pointer my-2  text-sm py-3 bg-green-600 text-white rounded-lg transition duration-200 hover:scale-110">
+   بیشتر از ما بدانید
+      </button>
+    </div>
+  </div>
+
+</div>
+
          
         </>
     )
---
+++ src/app/(store)/home/sliderProjects.jsx
@@ -0,0 +1,50 @@
+"use client";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Scrollbar } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import Image from 'next/image';
+
+export default function SliderProjects() {
+  const Images = [
+    { image: "01", text: "پروژه کاری 1" },
+    { image: "02", text: "پروژه کاری 2" },
+    { image: "03", text: "پروژه کاری 3" },
+    { image: "04", text: "پروژه کاری 4" },
+    { image: "01", text: "پروژه کاری 1" },
+    { image: "02", text: "پروژه کاری 2" },
+    { image: "03", text: "پروژه کاری 3" },
+  ];
+
+  return (
+    <div className="py-5">
+      <Swiper
+        scrollbar={{ hide: true }}
+        slidesPerView={4}
+        spaceBetween={20}
+        modules={[Scrollbar]}
+        className="mySwiper h-[400px]"
+      >
+        {Images.map((image, index) => (
+<SwiperSlide key={index}>
+  <div className="w-full h-full relative group overflow-hidden rounded-lg shadow-md">
+    <Image
+      src={`/images/projectsImage/protfolio-img${image.image}.png`}
+      width={400}
+      height={300}
+      alt="project"
+      className="w-full h-full object-cover"
+    />
+    <div className="absolute top-0 left-0 w-full h-full bg-black/80 translate-y-full group-hover:translate-y-0 transition-all duration-300 flex items-center justify-center text-white text-lg font-bold">
+      {image.text}
+    </div>
+  </div>
+</SwiperSlide>
+
+        ))}
+      </Swiper>
+    </div>
+  );
+}
