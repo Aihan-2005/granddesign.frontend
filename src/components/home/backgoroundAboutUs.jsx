a9b6b758165114a0f726610165a3d7dcd39eaaae
Aihan <mojtaba.shabani2580@gmail.com>
2025-08-11 22:30:56 +0330

    about part

--- src/components/home/backgoroundAboutUs.jsx
+++ src/components/home/backgoroundAboutUs.jsx
@@ -1,36 +1,79 @@
-export default function BackgroundeAboutUs(){
+import { useRef, useState } from "react";
+import Image from "next/image";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
+export default function BackgroundeAboutUs() {
+  const [isPlaying, setIsPlaying] = useState(false);
 
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
 
-    return (
-        <>
-<div    className="w-full  h-auto my-5 relative flex flex-col md:flex-row justify-center items-end  " dir="rtl">
+  const Information = [
+    { title: "طراحی داخل", number: 5321 },
+    { title: "طراحی نما", number: 3300 },
+    { title: "بازسازی خانه", number: 1900 },
+    { title: "پروژه های  کامل", number: 2421 },
+  ];
 
+  return (
+    <div
+      className="w-full h-auto my-8 relative flex flex-col md:flex-row justify-center items-end "
+      dir="rtl"
+    >
       <img
-    src="images/aboutimage.png"
-    alt="About Us"
-    className="w-full md:w-[40%] h-full  object-cover"
-  />
-  <div className="w-full md:w-[60-%] h-full flex justify-between items-center">
-    <div className="w-full text-right  px-5 relative mb-40 ">
-<p className="text-xl sm:text-2xl md:text-3xl  my-1 text-green-500 font-bold py-3 drop-shadow-[2px_2px_0px_#fff]">
-  درباره ما
-</p>      <p className="text-4xl my-1 text-[#e4e4e4] py-3">چرا ما متفاوت هستیم</p>
-      <p className="text-white text-base sm:text-lg md:text-xl w-[100%]  pl-[19%] py-3 border-r-[2px] px-2 border-[#e31e1e]">
-        تجربه‌ای خاص و متفاوت با تیم درجه یک و با تجربه، به کمک بروزترین ابزارهای دنیا بهترین خانه‌ها را با ما تجربه کنید.
-      </p>
-      <p className="text-white  text-base sm:text-lg md:text-xl w-[100%]  pl-[19%] py-3 px-2">
-      حس خانه ای مدرن با کمک ابزار بروز دنیا همچون هوشمصنوعی خانه هایی خاص خوتهیم ساخت مه در ایران نمییتوانید ببینید
-      </p>
-      <p className="text-white  text-base sm:text-lg md:text-xl w-[100%]  pl-[19%] py-3 px-2">
-       حس ها جدید و دل انگیز با کمک خانه های مدرن و ایده های ما برای هرچه زیبا تر کردن زندگی شما ، مارا انتخاب کنید همچون یک دوست
-      </p>
+        src="images/aboutimage.png"
+        alt="About Us"
+        className="w-full md:w-[40%] h-full object-cover"
+      />
+      <div className="w-full md:w-[60%] h-full flex justify-between md:items-start items-center  md:mb-40">
+        <div className="w-full text-right px-8 relative mb-5 py-2 md:mb-12">
+          <p className="text-xl sm:text-2xl md:text-3xl my-1 text-green-500 font-bold py-3 drop-shadow-[2px_2px_0px_#fff]">
+            درباره ما
+          </p>
+          <p className="text-4xl my-1 text-[#e4e4e4] py-3">
+            چرا ما متفاوت هستیم؟
+          </p>
+          <p className="text-white text-base sm:text-lg md:text-xl w-full pl-2  py-5 md:pl-[10%] border-r-3 px-2 border-r-teal-700">
+            تیم ما با بهره‌گیری از به‌روزترین تکنولوژی‌ها و دانش روز دنیا،
+            تجربه‌ای منحصر‌به‌فرد و خلاقانه در طراحی فضاهای زندگی ارائه می‌دهد.
+          </p>
+          <p className="text-white text-base sm:text-lg md:text-xl w-full pl-2  py-5 md:pl-[10%] ">
+            تیم ما با بهره‌گیری از به‌روزترین تکنولوژی‌ها و دانش روز دنیا،
+            تجربه‌ای منحصر‌به‌فرد و خلاقانه در طراحی فضاهای زندگی ارائه می‌دهد.
+          </p>
+          <p className="text-white text-base sm:text-lg md:text-xl w-full pl-2  py-4 md:pl-[10%] ">
+            تیم ما با بهره‌گیری از به‌روزترین تکنولوژی‌ها و دانش روز دنیا،
+            تجربه‌ای منحصر‌به‌فرد و خلاقانه در طراحی فضاهای زندگی ارائه می‌دهد.
+          </p>
+        </div>
+      </div>
+      <div
+        ref={ref}
+        className={`${
+          !isPlaying ? "opacity-100" : "opacity-0"
+        } absolute bottom-0 md:left-40  mx-auto md:mx-0
+            w-full md:w-[600px] max-w-6xl bg-white/10 backdrop-blur-xl
+            rounded-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4
+            text-white z-20 p-3  border border-white/20`}
+      >
+        {Information.map((item, index) => (
+          <div
+            key={index}
+            className="text-center flex flex-col items-center gap-1"
+            dir="rtl"
+          >
+            <p className="text-xs sm:text-sm md:text-base font-bold">
+              {item.title}
+            </p>
+            <p className="text-sm text-green-500 sm:text-sm md:text-base font-bold">
+              {inView ? <CountUp end={item.number} duration={2} /> : 0}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-
-</div>
-
-         
-        </>
-    )
+  );
 }
